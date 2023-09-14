a831982abb455383e09f9a09319c5aa383fd249d
Roberto-Berrios <robertoberrios70@gmail.com>
2023-09-13 22:33:07 -0600

    suppliers/crud

--- app/Http/Controllers/Dashboard/SupplierController.php
+++ app/Http/Controllers/Dashboard/SupplierController.php
@@ -9,71 +9,33 @@ use Illuminate\Http\Request;
 
 class SupplierController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
-        //
         return inertia('Dashboard/Supplier/Index', [
-            'suppliers' => Supplier::all()
+            'suppliers' => Supplier::all(),
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-        return inertia('Dashboard/Supplier/Create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(SupplierRequest $request)
     {
-        //
-        Supplier::create([
-            'name' => $request->name,
-            'contact' => $request->contact,
-            'ruc' => $request->ruc
-        ]);
+        Supplier::create($request->validated());
+
+        return back();
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Supplier $supplier)
+    public function update(SupplierRequest $request, $supplier)
     {
-        //
-        return inertia('Dashboard/Supplier/Show', [
-            'supplier' => $supplier
-        ]);
+        Supplier::where('id', $supplier)->update($request->validated());
+
+        return back();
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Supplier $supplier)
+  
+    public function destroy($supplier)
     {
-        //
-    }
+        Supplier::where('id', $supplier)->delete();
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Supplier $supplier)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Supplier $supplier)
-    {
-        //
+        return back();
     }
 }
---
+++ resources/js/Pages/Dashboard/Supplier/Index.vue
@@ -0,0 +1,146 @@
+<template>
+    <AppLayout title="Suppliers" :breads="breads">
+
+        <template #header>
+            <span class="title">
+                Suppliers
+            </span>
+            <AddButton @click="openModal = true" />
+        </template>
+
+        <TableSection>
+            <template #header>
+                <th>Nombre</th>
+                <th>Contacto</th>
+                <th>RUC</th>
+                <th>Accciones</th>
+            </template>
+
+            <template #body>
+                <tr class="hover:bg-gray-50" v-for="supplier in suppliers">
+                    <td>
+                        <span class="font-semibold">{{ supplier.name }}</span>
+                    </td>
+                    <td>
+                        {{ supplier.contact }}
+                    </td>
+                    <td>
+                        {{ supplier.ruc }}
+                    </td>
+                    <td>
+                        <div class="flex gap-4">
+                            <IconPencil role="button" @click="edit(supplier)"/>
+	                        <IconTrash role="button" @click="destroy(supplier.id)" />
+                        </div>
+                    </td>
+                </tr>
+                <tr v-if="suppliers.length == 0">
+                    <td colspan="4" class="text-center">No data to display</td>
+                </tr>
+            </template>
+        </TableSection>
+
+        <FormModal :show="openModal" title="Suppliers" @onCancel="resetValues()" @onSubmit="action">
+            <InputForm text="Name" v-model="form.name" />
+            <InputForm text="Contact" v-model="form.contact" />
+            <InputForm text="RUC" v-model="form.ruc" />
+        </FormModal>
+    </AppLayout>
+</template>
+
+<script setup>
+import AddButton from '@/Components/Buttons/AddButton.vue';
+import AppLayout from '@/Layouts/AppLayout.vue';
+import FormModal from '@/Components/Modal/FormModal.vue';
+import InputForm from '@/Components/Form/InputForm.vue';
+import { ref } from 'vue';
+import { useForm } from '@inertiajs/vue3';
+import { toast } from '@/Use/toast';
+import TableSection from '@/Components/TableSection.vue';
+import { IconPencil } from '@tabler/icons-vue';
+import { IconTrash } from '@tabler/icons-vue';
+import { confirmAlert } from '@/Use/helpers';
+
+defineProps({
+    suppliers: {
+        type: Object,
+        required: true,
+    }
+});
+
+const breads = [
+    {
+        name: 'Home',
+        route: route('dashboard.users.index'),
+    },
+    {
+        name: 'Suppliers',
+        route: route('dashboard.suppliers.index'),
+    },
+];
+
+const isNew = ref(true);
+
+const form = useForm({
+    id: '',
+    name: '',
+    contact: '',
+    ruc: '',
+});
+
+const openModal = ref(false);
+
+function edit(supplier) {
+    form.id = supplier.id;
+    form.name = supplier.name;
+    form.contact = supplier.contact;
+    form.ruc = supplier.ruc;
+    openModal.value = true;
+    isNew.value = false;
+}
+
+const action = () => isNew.value ? store() : update();
+
+function store() {
+    form.post(route('dashboard.suppliers.store'), {
+        preserveScroll: true,
+        preserveState: true,
+        onSuccess: () => {
+            toast.success('Saved successfully');
+            resetValues()
+        },
+    });
+}
+
+function update() {
+    form.put(route('dashboard.suppliers.update', form.id), {
+        preserveScroll: true,
+        preserveState: true,
+        onSuccess: () => {
+            toast.success('Updated successfully');
+            resetValues()
+        },
+    });
+}
+
+function resetValues() {
+    openModal.value = false;
+    isNew.value = true
+    form.reset();
+}
+
+function destroy(id) {
+    confirmAlert({
+        message: 'Are you sure you want to delete this category?',
+        onConfirm: () => {
+            form.delete(route('dashboard.suppliers.destroy', id), {
+                preserveScroll: true,
+                preserveState: true,
+                onSuccess: () => {
+                    toast.success('Deleted successfully');
+                },
+            });
+        },
+    })
+}
+</script>
