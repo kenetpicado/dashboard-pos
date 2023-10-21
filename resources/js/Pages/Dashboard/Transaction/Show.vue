16ef2f37aaa4f9d9a2253ef893c709ba077e1577
Kenet Picado <kenetpicado1@gmail.com>
2023-10-20 22:00:55 -0600

    Update Show.vue

--- resources/js/Pages/Dashboard/Transaction/Show.vue
+++ resources/js/Pages/Dashboard/Transaction/Show.vue
@@ -7,13 +7,26 @@
             </span>
         </template>
 
-        <div class="mb-4">
-            <div class="grid grid-cols-5 gap-4">
-                <StatCard v-for="stat in stats" :stat="stat" :key="stat.title" />
-            </div>
-        </div>
-
         <TableSection>
+            <template #options>
+                <div class="p-4 space-y-3 text-gray-600">
+                    <div>
+                        Fecha: {{ Carbon.create(transaction.created_at).format('d/m/Y H:i') }}
+                    </div>
+                    <div>
+                        Factura: #{{ transaction.id }}
+                    </div>
+                    <div>
+                        Tipo: {{ transactionTypes[transaction.type] }}
+                    </div>
+                    <div>
+                        Estado: {{ transaction.status }}
+                    </div>
+                    <div>
+                        Responsable: {{ transaction.user?.name }}
+                    </div>
+                </div>
+            </template>
             <template #header>
                 <th>Item</th>
                 <th>Medida</th>
@@ -84,8 +97,6 @@
 import AppLayout from '@/Layouts/AppLayout.vue';
 import TableSection from '@/Components/TableSection.vue';
 import { Carbon } from '@/Use/Carbon';
-import StatCard from '@/Components/StatCard.vue';
-import { IconCurrencyDollar } from '@tabler/icons-vue';
 
 const props = defineProps({
     transaction: {
@@ -113,24 +124,5 @@ const breads = [
         route: route('dashboard.transactions.show', props.transaction.id),
     }
 ];
-
-const stats = [
-    {
-        title: 'Factura',
-        value: props.transaction.id,
-    },
-    {
-        title: 'Tipo',
-        value: transactionTypes[props.transaction.type],
-    },
-    {
-        title: 'Estado',
-        value: props.transaction.status,
-    },
-    {
-        title: 'Fecha',
-        value: Carbon.create(props.transaction.created_at).format('d/m/Y H:i'),
-    },
-]
 
 </script>
